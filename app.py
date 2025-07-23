700ba872d938c1ed01237fcda1b560ef1d87d08b
q199255 <q199255@gmail.com>
2025-07-23 09:38:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, Response
 import requests
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def stream():
-    url = "https://375213f059a3.ngrok-free.app/stream.mp3"  # 改這裡
+    url = os.environ.get("STREAM_URL", "https://375213f059a3.ngrok-free.app/stream.mp3")
     headers = {
-        'ngrok-skip-browser-warning': '1'  # 用來跳過 Ngrok 防護頁面
+        'ngrok-skip-browser-warning': '1'
     }
     r = requests.get(url, headers=headers, stream=True)
     return Response(r.iter_content(chunk_size=1024), content_type=r.headers['Content-Type'])
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
